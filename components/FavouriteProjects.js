d967148a1f41754da7839535dc60831b3d497099
seydinah <seydinah94@gmail.com>
2022-03-21 20:18:19 +0000

    ajout folio

---
+++ components/FavouriteProjects.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+export default function FavouriteProjects() {
+  return (
+    <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
+      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2x1">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/seydi.jpg" alt="Man looking at item at a store"/>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ContainerBlock from "../components/ContainerBlock";
+import Hero from "../components/Hero";
+import FavouriteProjects from "@components/FavouriteProjects";
+import userData from "@constants/data";
+
+export default function Home() {
+  return (
+    <ContainerBlock
+      title="SeyTech - Developer, Engineer"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      {/*<FavouriteProjects />
+<LatestCode repositories={repositories} />*/}
+    </ContainerBlock>
+  );
+}
